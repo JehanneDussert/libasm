cec31e4e589b3fdf4f723f7553d3c88571dca95d
JehanneDussert <jehanne-dussert@hotmail.fr>
2020-03-28 18:40:31 +0100

    LibASM

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -4,9 +4,9 @@ extern _malloc
 extern _ft_strlen
 extern _ft_strcpy
 _ft_strdup :
-    push rdi ; i
+    push rdi ; index i
     call _ft_strlen
-    inc rax ; pour le \0
+    inc rax ; on prevoit un espace en plus pour le \0
     mov rdi, rax
     call _malloc
     pop rsi
