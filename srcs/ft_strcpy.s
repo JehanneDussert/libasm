d562478baa7f1a3f4cdfdde6246967d75d87cadb
JehanneDussert <jehanne-dussert@hotmail.fr>
2020-03-28 18:37:38 +0100

    LibASM

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,19 +1,15 @@
 global _ft_strcpy
     section .text
 _ft_strcpy :
-    push rbx ; i
-    push rcx ; src
-    mov rbx, -1
-    mov rcx, rsi
-    mov rax, rdi ; dst
+    push rcx ; i
+    mov rcx, -1
 _ft_cpy :
-    inc rbx
-    mov dl, BYTE [rcx + rbx] ; dl = tmp
-    mov BYTE [rax + rbx], dl ; on remplit la dst
+    inc rcx
+    mov dl, BYTE [rsi + rcx] ; dl = tmp
+    mov BYTE [rdi + rcx], dl ; on remplit la dst
     cmp dl, 0 ; on verifie que ca n'est pas le dernier char
-    je _ft_ret ; si oui = ret
     jne _ft_cpy ; si non = on continue le remplissage
 _ft_ret  :
-    pop rbx ; on libere l'espace de i + src
-    pop rcx
+    mov rax, rdi
+    pop rcx ; on libere l'espace
     ret
